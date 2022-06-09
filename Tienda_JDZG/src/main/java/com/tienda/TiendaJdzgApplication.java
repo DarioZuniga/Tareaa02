b0df88bb6e09d6ff037584a4d149f25654fae6e1
DarioZuniga <jzuniga50539@ufide.ac.cr>
2022-06-08 21:01:55 -0600

    Revert "0.0.3"
    This reverts commit a83fba75ebeac348329eaa5b47f30d19d4590d43.

---
+++ Tienda_JDZG/src/main/java/com/tienda/TiendaJdzgApplication.java
@@ -0,0 +1,24 @@
+package com.tienda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaJdzgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaJdzgApplication.class, args);
+	}
+//hvcvscnbsncbsnc
+        
+        
+        
+        
+        
+        
+        
+        
+        
+//        chdcjhvadckjbadcj
+//        dvsdcghvswwjdhbq
+}
